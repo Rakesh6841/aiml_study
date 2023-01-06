0154eed8adaa24a51031620c6a3cd6f1f279f1b6
Rakesh Kundu <57815079+Rakesh6841@users.noreply.github.com>
2023-01-06 22:37:34 +0530

    Update 1_a_star.py

--- 1_a_star.py
+++ 1_a_star.py
@@ -81,33 +81,7 @@ def get_neighbors(v):
 #and this function returns heuristic distance for all nodes
  
 def heuristic(n):
-    H_dist = {
-        'A': 10,
-        'B': 8,
-        'C': 5,
-        'D': 7,
-        'E': 3,
-        'F': 6,
-        'G': 5,
-        'H': 3,
-        'I': 1,
-        'J': 0
-    }
-
+    H_dist = {'A':1,'B':1, 'C':1,'D':1}
     return H_dist[n]
-
-#Describe your graph here
-Graph_nodes = {
-    
-    'A': [('B', 6), ('F', 3)],
-    'B': [('C', 3), ('D', 2)],
-    'C': [('D', 1), ('E', 5)],
-    'D': [('C', 1), ('E', 8)],
-    'E': [('I', 5), ('J', 5)],
-    'F': [('G', 1),('H', 7)] ,
-    'G': [('I', 3)],
-    'H': [('I', 2)],
-    'I': [('E', 5), ('J', 3)],
-
-}
-aStarAlgo('A', 'J')
+Graph_nodes = {'A':[('B',1),('C',3),('D',7)],'B':[('D',5)],'C':[('D',12)]}
+aStarAlgo('A', 'D')
